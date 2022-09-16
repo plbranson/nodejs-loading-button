1d1a77d89da4c186b5d726331d154247fe3e4329
plbranson <patrick.l.branson@plbranson.com>
2022-09-15 21:58:15 -0400

    Finished product

---
+++ public/javascripts/script.js
@@ -0,0 +1,6 @@
+const searchButton = document.querySelector("#search-button");
+const searchButtonContent = document.querySelector("#search-button div");
+
+searchButton.addEventListener("click", () => {
+  searchButtonContent.classList.toggle("loading");
+});
